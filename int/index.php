eab45bfae2878b01d1c605bc76c58bd961717a86
A-Kija <42836152+A-Kija@users.noreply.github.com>
2020-02-20 11:59:14 +0200

    img

--- int/index.php
+++ int/index.php
@@ -1,8 +1,17 @@
 <?php
 use Miskai\Miskas2;
+use Intervention\Image\ImageManagerStatic as Image;
 
 require __DIR__.'/../vendor/autoload.php';
 
 $miskas = new Miskas2;
 
-echo $miskas->visi_gyvunai();
+// echo $miskas->visi_gyvunai();
+
+$img = Image::make('img.jpg');
+$img->resize(rand(100, 300), rand(100, 300));
+$img->save('galutinis.jpg');
+
+header('Content-type: image/jpeg');
+
+echo file_get_contents('galutinis.jpg');
